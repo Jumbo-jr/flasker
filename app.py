442753f7d35356f0defbdffa9866bf938630aa1b
Jumbo-jr <w.wongnin@gmail.com>
2022-03-03 18:52:03 +0700

    little app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def user(name):
 def page_not_found(e):
     return render_template('404.html'), 404
 
-#Internal Server
+#Internal Server error
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template('500.html'), 500
